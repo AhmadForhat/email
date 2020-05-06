6e9da3cb5b228974ad74a72b98d73f509a08abd3
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-05-06 15:39:03 -0300

    change middy

--- src/templates/main.js
+++ src/templates/main.js
@@ -1,16 +1,20 @@
 require('dotenv').config()
 const middy = require('@middy/core')
-const { basicAuth } = require('@ziro/middleware')
-const httpJsonBodyParser = require('@middy/http-json-body-parser')
-const httpErrorHandler = require('@middy/http-error-handler')
-const createError = require('http-errors')
-const cors = require('@middy/http-cors')
+const jsonBodyParser = require('@middy/http-json-body-parser')
+const { preflight } = require('@ziro/middleware')
+const { allowedOrigin } = require('@ziro/middleware')
+const { auth } = require('@ziro/middleware')
+const { errorHandler } = require('@ziro/middleware')
+const { cors } = require('@ziro/middleware')
+const allowed = null
 
-const main = handler =>
+const lambda = handler =>
     middy(handler)
-        .use(basicAuth)
-        .use(httpJsonBodyParser())
-        .use(httpErrorHandler())
-        .use(cors())
+        .use(preflight)
+        .use(allowedOrigin(allowed))
+        .use(auth)
+        .use(jsonBodyParser())
+        .use(errorHandler)
+        .use(cors)
 
-module.exports = main
+module.exports = lambda
